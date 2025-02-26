b3a1bd24350ef52ca2faecc9b44002fc5e4004c2
keyurbhatiya <keyurbhatiya3004@gamil.com>
2025-02-26 22:31:41 +0530

    Day1/180

---
+++ Basics of Python/Day1.py
@@ -0,0 +1,20 @@
+# Introduction to Python, Installation, and Setup
+
+
+'''
+What is Python
+
+Python is a general-purpose, dynamically typed, high-level, compiled 
+and interpreted, garbage-collected, and purely object-oriented programming 
+language that supports procedural,object-oriented, and functional programming.
+
+'''
+
+# Python Program
+print("Hello World!")
+
+
+'''
+Visit the link https://www.python.org to download the latest release of Python. In this process, we will install Python 3.11.3 on our Windows operating system.
+'''
+
