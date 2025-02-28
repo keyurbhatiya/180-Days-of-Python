f737a6a176295c522451bd4854ea4ea9494c211a
keyurbhatiya <keyurbhatiya3004@gamil.com>
2025-02-28 23:16:12 +0530

    3/180

---
+++ Basics of Python/Day3.py
@@ -0,0 +1,93 @@
+#  Day 3: Operators (Arithmetic, Logical, Comparison, Bitwise)
+
+
+'''
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+
+'''
+
+
+# Arithmetic Operators
+
+'''
++	Addition
+-	Subtraction
+*	Multiplication
+/	Division
+%	Modulus
+**	Exponentiation
+//	Floor division
+'''
+print("Arithmetic Operators")
+print(10+5)
+print(10-5)
+print(10*5)
+print(10/5)
+print(10%5)
+print(10**5)
+print(10//5)
+
+
+# Logical 
+'''
+
+and
+or
+not
+
+'''
+print("Logical Operators")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+
+# Comparison
+
+'''
+==	Equal
+!=	Not equal
+>	Greater than
+<	Less than
+>=	Greater than or equal to
+<=	Less than or equal to
+'''
+print("Comparison Operators")
+print(10==10)
+print(10!=10)
+print(10>10)
+print(10<10)
+print(10>=10)
+print(10<=10)
+
+
+# Bitwise
+
+'''
+&	Bitwise AND
+|	Bitwise OR
+^	Bitwise XOR
+~	Bitwise NOT
+<<	Bitwise left shift
+>>	Bitwise right shift
+'''
+print("Bitwise Operators")
+print(10&10)
+print(10|10)
+print(10^10)
+print(~10)
+print(10<<10)
+print(10>>10)
+
