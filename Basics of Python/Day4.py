d5adfe840b6cafa7e51af1974a90b2a548ffb06c
keyurbhatiya <keyurbhatiya3004@gamil.com>
2025-03-01 14:06:12 +0530

    4/180

---
+++ Basics of Python/Day4.py
@@ -0,0 +1,35 @@
+# Day 4: Conditional Statements (if, elif, else)
+
+
+# Conaditional Statements
+
+'''
+if
+elif
+else
+
+'''
+
+# if Statement
+print("if Statement")
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+# elif Statement
+print("elif Statement")
+x = 10
+if x > 5:
+    print("x is greater than 5")
+elif x < 5:
+    print("x is less than 5")
+
+# else Statement
+print("else Statement")
+x = 10
+if x > 5:
+    print("x is greater than 5")
+elif x < 5:
+    print("x is less than 5")
+else:
+    print("x is equal to 5")
